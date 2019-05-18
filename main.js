f467aabb983294ae758da049f0d6ca321a2b10bb
Michael Crenshaw <mkrenshaw96@gmail.com>
2019-05-18 17:49:44 -0400

    Third commit of May 18th

--- main.js
+++ main.js
@@ -1,46 +1,86 @@
 let baseURL = 'https://api.spacexdata.com/v3/launches/next';
-// let img = document.querySelector('img.wallpaper');
-// let img = document.querySelector('div.view');
 let numDays = document.querySelector('div.num-days');
 let numHours = document.querySelector('div.num-hours');
 let numMins = document.querySelector('div.num-mins');
 let numSec = document.querySelector('div.num-secs');
 let headingDiv = document.querySelector('div.heading');
+let about = document.querySelector('div.about-content');
+let rocketId = document.querySelector('div.rock-id');
+let rocketType = document.querySelector('div.rock-type');
+let rocketRecov = document.querySelector('div.rock-recov');
+let rocketReuse = document.querySelector('div.rock-reuse')
+let liveVideo = document.querySelector('iframe.live');
+let payType = document.querySelector('div.pay-type');
+let payMass = document.querySelector('div.pay-mass');
+let payOrb = document.querySelector('div.pay-orb')
+let payOrbType = document.querySelector('div.pay-orb-type')
 let missionName;
 let newName;
-let about = document.querySelector('div.about-content');
 let launchDateUnix;
-let countdown;
-let liveVideo;
-let rocketImage;
-let upcomingLaunchInfo;
-let endTime;
+let lbs = ' lbs';
 
 fetch(baseURL)
     .then(function (response) {
         return response.json();
     })
     .then(function (json) {
-        console.log(json)
+        // console.log(json)
         displayResults(json);
     })
 
 function displayResults(json) {
-    upcomingLaunchInfo = json;
     //ABOUT PARAGRAPH
-    let details = upcomingLaunchInfo.details;
+    let details = json.details;
     let cont = document.createElement('div');
     cont.textContent = details;
     about.appendChild(cont)
+
     //MISSION NAME 
-    missionName = upcomingLaunchInfo.mission_name;
+    missionName = json.mission_name;
     newName = missionName.slice(0, -7)
     let title = document.createElement('div');
     title.textContent = newName;
     headingDiv.appendChild(title);
+
     //COUNTDOWN UNIX TIMER
-    launchDateUnix = upcomingLaunchInfo.launch_date_unix;
+    launchDateUnix = json.launch_date_unix;
     timer(launchDateUnix);
+
+    //ROCKET ID
+    let id = json.rocket.rocket_id;
+    rocketId.textContent = id;
+
+    //ROCKET TYPE
+    let type = json.rocket.rocket_type;
+    rocketType.textContent = type;
+
+    //ROCKET RECOVERY
+    let recov = json.rocket.fairings.recovery_attempt;
+    rocketRecov.textContent = recov;
+
+    //ROCKET REUSE
+    let reuse = json.rocket.fairings.reused;
+    rocketReuse.textContent = reuse;
+
+    //LIVE VIDEO
+    let live = json.links.video_link;
+    liveVideo.src = live;
+
+    //PAYLOAD TYPE
+    let pType = json.rocket.second_stage.payloads[0].payload_type;
+    payType.textContent = pType;
+
+    //PAYLOAD MASS
+    let mass = json.rocket.second_stage.payloads[0].payload_mass_lbs;
+    payMass.textContent = mass + lbs;
+
+    //PAYLOAD ORBIT
+    let orbit = json.rocket.second_stage.payloads[0].orbit_params.reference_system;
+    payOrb.textContent = orbit;
+
+    //PAYLOAD ORBIT TYPE
+    let orbitType = json.rocket.second_stage.payloads[0].orbit
+    payOrbType.textContent = orbitType;
 }
 
 function timer() {
@@ -60,11 +100,11 @@ function timer() {
         numSec.textContent = seconds;
         if (t < 0) {
             // clearInterval(x);
-
         }
     }, );
 
 }
+
 $("nav").find("a").click(function (e) {
     e.preventDefault();
     var section = $(this).attr("href");
